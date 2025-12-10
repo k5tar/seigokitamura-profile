17fd8c6d035275846720a40a79c95a2162b3ec90
Seigo Kitamura <k5tar4biz@gmail.com>
2025-12-11 00:11:42 +0900

    modifed next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    output: "export", // Static HTML export
+    output: 'export',
     basePath: isProd ? '/seigokitamura-profile' : '',
     assetPrefix: isProd ? '/seigokitamura-profile/' : '',
 };
